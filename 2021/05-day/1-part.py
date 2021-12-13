37abe80542b426d311b41a5bb903dff6fd0ff208
Pascal Salvador <smv7.github@gmail.com>
2021-12-13 17:06:23 -0300

    Adding largest coordinate finder function

--- 2021/05-day/1-part.py
+++ 2021/05-day/1-part.py
@@ -1,7 +1,11 @@
 '''12-13-2021, AdventCode day 5 part 1 by smv7'''
 
+from modules import create_dots_map
+from modules import find_largest_coordinate
+
 
 def main():
+    # Transforming the input to Python Data Structure.
     file_path = './input.txt'
     vent_lines = []
     with open(file_path) as f:
@@ -10,7 +14,10 @@ def main():
             coor_1, coor_2 = line.split(' -> ') # Coordinates (x1,y1) and (x2,y2)
             vent_line = (tuple(coor_1.split(',')), tuple(coor_2.split(',')))
             vent_lines.append(vent_line)
-    print(vent_lines)
+
+    # Creating the dots map
+    largest_x, largest_y = find_largest_coordinate(vent_lines)
+    dots_map = create_dots_map(largest_x, largest_y)
 
 
 if __name__ == '__main__':
--- 2021/05-day/modules.py
+++ 2021/05-day/modules.py
@@ -11,3 +11,20 @@ def create_dots_map(x:int, y:int) -> list:
         i += 1
 
     return dots_map
+
+
+def find_largest_coordinate(sequence: list) -> tuple:
+    '''Return a tuple with the largest numbers of 'x' coordinates (index 0) and 'y' (index 1)'''
+    
+    largest_x = 0
+    largest_y = 0
+    for outer in sequence:
+        for inner in outer:
+            x = int(inner[0])
+            y = int(inner[1])
+            if x > largest_x:
+                largest_x = x
+            if y > largest_y:
+                largest_y = y
+
+    return (largest_x, largest_y)
